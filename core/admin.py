2d60747d731a5cec53273735d65fc7f7cda978dd
Drackex <nic.rojasr@duocuc.cl>
2023-01-15 21:35:28 -0300

    BBDDintegrada

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Tipos_Empleados)
+admin.site.register(Empleado)
+admin.site.register(Cliente)
+admin.site.register(Estado_Compras)
+admin.site.register(Ventas)
+admin.site.register(Devoluciones)
+admin.site.register(Categoria)
+admin.site.register(Clase)
+admin.site.register(Producto)
+admin.site.register(Tienda)
