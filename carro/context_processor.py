2fa6301df105d18ba5885d0495b70e61c5467cc9
beramirezduocuc <be.ramirez@duocuc.cl>
2023-01-20 19:09:10 -0300

    Api funcionando, la rama anterior tenia error

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,13 +1,14 @@
+from .carro import Carro
 def importe_total_carro(request):
+    carro = Carro(request)
     total = 0
-    for key,value in request.session["carro"].items():
+    for key,value in carro.carro.items():
         total = total +(float(value["precio"])*value["cantidad"])
-
     return {"importe_total_carro":total}
 
 def subtotal(request):
+    carro = Carro(request)
     total = 0
-    for key,value in request.session["carro"].items():
+    for key,value in carro.carro.items():
         total = total +(float(value["precio"])*value["cantidad"])
-
     return {"importe_total_carro":total}
